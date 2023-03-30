59ad2c4f86f23c067768a4e419ee5d9f4e360736
Thomas Braun <thomas.braun@byte-physics.de>
2023-03-30 20:57:37 +0200

    Add copybutton extension
    This allows to conveniently copy text in code blocks. See
    https://github.com/executablebooks/sphinx-copybutton.

--- source/conf.py
+++ source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
     'tangocontrib.metalabels',
 #    'sphinx.ext.autosectionlabel',
 ]
