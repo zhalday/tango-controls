2a552c618a8bd531c652e15c8b3e7b84c9fb1e7d
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2025-02-06 13:32:51 +0100

    Update footer, and link to copyright license
    - add empty author.html to avoid "By Author name not set"
    - create copyright.html to add link to CC license

--- source/conf.py
+++ source/conf.py
@@ -96,7 +96,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Tango Controls"
-copyright = "2017-2025, Tango Community, Creative Commons Attribution 4.0 International (CC BY 4.0)"
+# copyright in _templates/copyright.html
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
